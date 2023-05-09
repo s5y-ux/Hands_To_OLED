8fe208940fcf1fe08d68400fb2ea81955e00f61e
Joseph Shumaker <59636597+s5y-ux@users.noreply.github.com>
2023-05-08 22:00:52 -0700

    Add files via upload

--- sketch_may04a.ino
+++ sketch_may04a.ino
@@ -10,46 +10,107 @@ conversion in the serial monitor and output data through the analog pins.
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+//Sets up constants for the OLED deminsions
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
+//instantiates Adafruit_SSD1306 object with respective parameters
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+//Used as a varible in which the OLED will reference and display
 String msg;
+
+//Initalization on power-up
 void setup() {
+
+  //Opens up the serial monitor for transfers of up to 9600 bits per second
    Serial.begin(9600);
-   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+
+   //Logic check to see if the OLED was detected (address 0x3D for 128x64)
+   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+
+     //If it fails, let the serial monitor know
     Serial.println(F("SSD1306 allocation failed"));
+
+    //Empty, "for" loop for delayed operation
     for(;;);
   }
+
+  //wait 2 seconds
   delay(2000);
+
+  //clear the data
   display.clearDisplay();
 }
+
+//After instantiation and OLED detection
 void loop() {
+
+  //Reads the data in the serial port and stores the data as "msg"
   readSerialPort();
+
+  //Sees if the message isent blank
   if (msg != "") {
+
+    //if its not then we will send the data to the OLED
       sendData();
   }
+
+  //then we wait a half second before reciving more data
   delay(500);
 }
+
+
+//referenced in loop() function for interpreting data in serial monitor
 void readSerialPort() {
+
+  //allocates global string, "msg" and sets its value to an empty string
   msg = "";
+
+  //Logic check on if data is avaliable on the searial monitor
   if (Serial.available()) {
+
+    //if its so we will delay for 1/100th of a second
       delay(10);
+
+      //and while the data is avaliable
       while (Serial.available() > 0) {
+
+        //we will concatinate the character to the string
           msg += (char)Serial.read();
       }
+
+      //and flush the data
       Serial.flush();
   }
 }
+
+//function used to actually display the message on the OLED
 void sendData() {
-  //write data
+  /*
+
+  Clears OLED
+  |
+  v
+  Sets Text Size
+  |
+  v
+  Sets Text Color
+  |
+  v
+  Sets Cursor Point
+  |
+  v
+  Prints OLED_String
+  |
+  v
+  Displays
+
+  */
   display.clearDisplay();
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  // Display static text
   display.println(msg);
   display.display();
-  //Serial.print(msg);
 }
